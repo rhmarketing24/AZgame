6aa4739db9a444897e32688f76d2663607840ba9
Md Raktiar Hossain <78993085+rhmarketing24@users.noreply.github.com>
2025-12-09 17:19:00 +0600

    Update game.js

--- pages/game.js
+++ pages/game.js
@@ -1,1 +1,6 @@
+import dynamic from 'next/dynamic';
+const GameScreen = dynamic(() => import('../components/GameScreen'), { ssr: false });
 
+export default function Game() {
+  return <GameScreen />;
+}

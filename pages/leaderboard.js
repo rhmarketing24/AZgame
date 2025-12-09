831deefe403f90b668ba5ebab34b05adcd6d5fb5
Md Raktiar Hossain <78993085+rhmarketing24@users.noreply.github.com>
2025-12-09 17:19:22 +0600

    Update leaderboard.js

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,1 +1,6 @@
+import dynamic from 'next/dynamic';
+const LeaderboardScreen = dynamic(() => import('../components/LeaderboardScreen'), { ssr: false });
 
+export default function Leaderboard() {
+  return <LeaderboardScreen />;
+}

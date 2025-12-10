84d8b2b9fd9e858f5f563d27d394ea52b71777e9
Md Raktiar Hossain <78993085+rhmarketing24@users.noreply.github.com>
2025-12-10 16:37:02 +0600

    Delete pages directory

--- pages/game.js
+++
@@ -1,8 +0,0 @@
-// pages/game.js
-import dynamic from "next/dynamic";
-
-const GameScreen = dynamic(() => import("../components/GameScreen"), { ssr: false });
-
-export default function Game() {
-  return <GameScreen />;
-}
